798d470bc67dfb28c98b9a35df588df575ef6228
waidhoferj <waidhoferj@gmail.com>
2019-11-20 12:00:47 -0800

    added gyro fsm

---
+++ sources_1/new/GyroFsm.sv
@@ -0,0 +1,15 @@
+module GyroFsm (
+    begin_transmission,
+    recieved_data,
+		end_transmission,
+		clk,
+		rst,
+		start,
+		slave_select,
+		send_data,
+		x_axis_data,
+		y_axis_data,
+		z_axis_data
+);
+    
+endmodule
