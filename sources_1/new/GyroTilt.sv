8595cd407b7d3205115e85019dbe88dfb43ac93d
waidhoferj <waidhoferj@gmail.com>
2019-11-22 13:39:18 -0800

    Added comments and initial conditions

--- sources_1/new/GyroTilt.sv
+++ sources_1/new/GyroTilt.sv
@@ -1,17 +1,49 @@
+// Module: GyroTilt
+//Purpose: Converts angular velocity data into tilt angle of gyro
+
+//Inputs
+    // dx - dz: the current angular velocity from GyroFsm
+    // RST: will set each dimension to 0 degree tilt
+//Outputs
+    // X-Z: the current tilt in each dimension
+
+
 module GyroTilt (
     input [15:0] dx, dy dz, //Angular velocity
     input RST,
-    output logic [15:0] X, Y
+    input CLK
+    output logic [15:0] X, Y, Z
 );
+
+//Internal Variables
+//=============================================
+logic [15:0] x, y, z
+
+//Functionality
+//=============================================
+initial begin
+    x = 0;
+    y = 0;
+    z = 0;
+    X <= 0;
+    Y <= 0;
+    Z <= 0;
+
+end
+
+always_ff @(posedge CLK) begin
 if(RST){
-    X = 0;
-Y = 0;
+    X <= 0;
+    Y <= 0;
+    Z <= 0;
 }else {
-x + dxt;
-logic rate = 10;
-logic x = x + dx*rate;
-logic y = y + dy*rate
-X = x;
-Y = y;
+    parameter rate = 10; //Amount of time passed (clock speed)
+    x = x + dx*rate;
+    y = y + dy*rate
+    z = z + dz*rate
+    assign X = x;
+    assign Y = y;
+    assign Z = z
 }
+end
 endmodule
