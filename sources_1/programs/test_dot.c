20f1e2a9f367ca260244adfbd5626c561697dba1
Waidhoferj <jwaidhof@calpoly.edu>
2019-12-06 16:41:15 -0800

    commented testDot

--- sources_1/programs/test_dot.c
+++ sources_1/programs/test_dot.c
@@ -1,7 +1,14 @@
+//PUROPSE: Test the gyroscope and button connections in an isolated and simple environment.
+//CREDIT: Used the OTTER VGA 80x60 library to draw to the screen
+
+//IMPORTED LIBS
+//======================================================================
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-const int BG_COLOR = 0xFF; // light blue (0/7 red, 3/7 green, 3/3 blue)
+
+//ADDRESSES
+//======================================================================
 volatile int *const VG_ADDR = (int *)0x11100000;
 volatile int *const VG_COLOR = (int *)0x11140000;
 volatile int *const GYRO_X = (int *)0x11080000;
@@ -13,15 +20,23 @@ volatile int *const BTN_RIGHT_ADDR = (int *)0x110c0000;
 volatile int *const BTN_TOP_ADDR = (int *)0x110d0000;
 volatile int *const BTN_BOTTOM_ADDR = (int *)0x110e0000;
 volatile int *const BTN_CENTER_ADDR = (int *)0x110f0000;
+
+//GLOBALS
+//======================================================================
+const int BG_COLOR = 0xFF; // light blue (0/7 red, 3/7 green, 3/3 blue)
 int const SCREEN_WIDTH = 80;
 int const SCREEN_HEIGHT = 60;
 
+//Prototypes
+//======================================================================
 static void draw_background();
 static void draw_dot(int X, int Y, int color);
 static void draw_horizontal_line(int X, int Y, int toX, int color);
 static void delay(int ms);
 static void print_SSEG(int num);
 
+//MAIN
+//======================================================================
 void main()
 {
     int x = SCREEN_WIDTH / 2;
@@ -37,6 +52,10 @@ void main()
         delay(100);
     }
 }
+
+//FUNCTIONS
+//======================================================================
+//Delays the main thread ms number of milliseconds.
 void delay(int ms)
 {
     int count = ms * 12500;
@@ -44,6 +63,11 @@ void delay(int ms)
     for (i = 0; i < count; i++)
     {
     };
+}
+//Prints number as hex to the seven segment display on the OTTER.
+static void print_SSEG(int num)
+{
+    *SSEG_ADDR = num;
 }
 
 static void draw_dot(int X, int Y, int color)
@@ -69,9 +93,4 @@ static void draw_horizontal_line(int X, int Y, int toX, int color)
     {
         draw_dot(X, Y, color);
     }
-}
-
-static void print_SSEG(int num)
-{
-    *SSEG_ADDR = num;
 }
